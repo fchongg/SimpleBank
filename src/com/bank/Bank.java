dbdacd0b727468454fd38fb6a90ae13c4424027f
fchongg <fchongg@gmail.com>
2017-09-29 22:21:45 -0700

    New stub updates

---
+++ src/com/bank/Bank.java
@@ -0,0 +1,24 @@
+package com.bank;
+import java.util.HashMap;
+
+public class Bank{
+    HashMap accounts;
+    public Bank(){
+        accounts = new HashMap();
+    }
+
+    public void addAccount(String name, String accountNum, String limit){
+    }
+
+    public void chargeAccount(String name, int value){
+
+    }
+
+    public void creditAccount(String name, int value){
+
+    }
+
+    public void printSummary(){
+
+    }
+}
