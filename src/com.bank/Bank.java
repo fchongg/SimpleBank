c2b84e7e79e747a8b2c3c1943066ccee8083dd20
fchongg <fchongg@gmail.com>
2017-09-29 22:12:14 -0700

    added stubs

---
+++ src/com.bank/Bank.java
@@ -0,0 +1,10 @@
+package com.bank;
+import java.util.HashMap;
+
+public class Bank{
+    HashMap accounts;
+    public Bank(){
+        accounts = new HashMap();
+    }
+
+}
---
+++ src/com.bank/Main.java
@@ -0,0 +1,10 @@
+package com.bank;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
