8850f0114b6195bb2dd11528b5920c40518dd6d6
U-NETAPP\fchong <fchong@MANSHANT01-PC.hq.netapp.com>
2017-10-04 19:28:39 -0700

    Updated functionality to handle Luhn credit cards

--- src/com/bank/Bank.java
+++ src/com/bank/Bank.java
@@ -1,6 +1,7 @@
 package com.bank;
 
 import java.util.*;
+import com.bank.Luhn;
 
 /**
  * Created by Frances on 2017-09-30.
@@ -17,50 +18,60 @@ public class Bank {
     // more information at: https://en.wikipedia.org/wiki/Luhn_algorithm
     public void createAccount(String name, String account_num, String limit){
         ArrayList<String> new_account = new ArrayList<String>();
+        boolean luhn_approved = Luhn.Check(account_num);
         new_account.add(account_num);
         new_account.add(limit);
-        new_account.add("0");
+        if(!luhn_approved){
+          // this sets the balance to error
+          new_account.add("error");
+        }
+        else {
+          new_account.add("0");
+        }
         accounts.put(name, new_account);
-
-        System.out.print(accounts.get(name).toString());
     }
 
     // This function debits the amount to the specified bank account
     // If the amount being debited will result in a balance greater than the
     // limit, then it will not be debited
+    // If it's a Luhn cc, then nothing will be done to the balance (error will persist)
     public void charges(String name, String amount){
         ArrayList<String> account_info = (ArrayList<String>) accounts.get(name);
+        if (account_info.get(2).equals("error")){
+          return;
+        }
         int limit = Integer.parseInt(account_info.get(1).substring(1));
         int balance = Integer.parseInt(account_info.get(2));
         int new_bal = balance + Integer.parseInt(amount.substring(1,amount.length()));
         if (new_bal <= limit){
             ((ArrayList<String>) accounts.get(name)).set(2, Integer.toString(new_bal));
         }
-
-        System.out.println(accounts.get(name).toString());
-
     }
 
     // This function credits the amount from the account o
-    // Credits against Luhn 10 cards will produce an error
+    // If it's a Luhn cc, then nothing will be done to the balance (error will persist)
     public void credit(String name, String amount){
         ArrayList<String> account_info = (ArrayList<String>) accounts.get(name);
+        if (account_info.get(2).equals("error")){
+          return;
+        }
         int balance = Integer.parseInt(account_info.get(2));
         int new_bal = balance - Integer.parseInt(amount.substring(1,amount.length()));
         ((ArrayList<String>) accounts.get(name)).set(2, Integer.toString(new_bal));
-
-        System.out.println(accounts.get(name).toString());
     }
 
     // This function prints out the summary statement
     public void printSummary(){
-        Iterator entries = accounts.entrySet().iterator();
-        while (entries.hasNext()) {
-            Map.Entry thisEntry = (Map.Entry) entries.next();
-            Object key = thisEntry.getKey();
-            ArrayList<String> value = (ArrayList<String>) thisEntry.getValue();
 
-            System.out.println(key + ": $" + value.get(2));
+        SortedSet<String> keys = new TreeSet<String>(accounts.keySet());
+        for (String key : keys) {
+            ArrayList<String> value = (ArrayList<String>) accounts.get(key);
+
+            String balance = value.get(2);
+            if(!balance.equals("error")){
+                balance = "$" + balance;
+            }
+            System.out.println(key + ": " + balance);
         }
     }
 
--- src/com/bank/Main.java
+++ src/com/bank/Main.java
@@ -8,21 +8,13 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         ReadFile rf = new ReadFile();
         List<String> lines = rf.readFile(args[0]);
 
-        System.out.println(lines);
         Bank new_bank = new Bank();
         for (int i = 0; i < lines.size(); i++){
             new_bank.doAction(lines.get(i));
         }
         new_bank.printSummary();
-//        new_bank.createAccount("Ben", "218397189", "$1000");
-//        new_bank.charges("Ben", "$20");
-//        new_bank.charges("Ben", "$800");
-//        new_bank.charges("Ben", "$200");
-//        new_bank.credit("Ben", "$50");
-//        new_bank.credit("Ben", "$1000");
     }
 }
